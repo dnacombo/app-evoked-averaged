7583c77ba3aea894b58cbfbe1938a7af0d1d18b2
SaeedZahran <saeedzahran@hotmail.com>
2022-05-01 21:46:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,37 +7,23 @@ import matplotlib.pyplot as plt
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def epoch(raw,tmin,tmax):
-
-    # extract an events array from Raw objects using mne.find_events():
-    # reading experimental events from a “STIM” channel;
-    events = mne.find_events(raw, stim_channel='STI 014')
+def evoked(epochs):
 
 
-    event_dict = {'auditory/left': 1, 'auditory/right': 2, 'visual/left': 3,
-                  'visual/right': 4}
-
-    epochs = mne.Epochs(raw, events, tmin=-0.3, tmax=0.7, event_id=event_dict,
-                        preload=True)
-
-    evoked = epochs['auditory/left'].average()
-    evoked.plot()
-
-
-
-
+    evoked = epochs.average()
+    
     # == SAVE FILE ==
     evoked.save(os.path.join('out_dir', 'evoked-epo.fif'))
 
     # == FIGURES ==
     plt.figure()
     evoked.plot()
-    plt.savefig(os.path.join('out_figs', 'voked_plot.png'))
+    plt.savefig(os.path.join('out_figs', 'evoked_plot.png'))
 
 
     
  
-    return epochs
+    return evoked
 
 
 
@@ -49,25 +35,14 @@ def main():
     with open('config.json') as config_json:
         config = json.load(config_json)
 
-    # Read the meg file
+    # Read the epoch file
     data_file = config.pop('fif')
 
-    # Read the event time
-    tmin = config.pop('t_min')
-    tmax = config.pop('t_max')
 
     # crop() the Raw data to save memory:
-    raw = mne.io.read_raw_fif(data_file, verbose=False).crop(tmax=60)
+    epochs = mne.io.read_raw_fif(data_file, verbose=False).crop(tmax=60)
 
-    ## Build epochs parameters
-    epochs_params = dict(tmin=tmin, tmax=tmax)
-
-    epochs = epoch(raw,**epochs_params)
-
-
-
-
-
+    evoked = evoked(epochs)
 
 
 if __name__ == '__main__':
