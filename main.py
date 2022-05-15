bb1029558ff11aa3a08b0453e1e5e7075e98c5c9
SaeedZahran <saeedzahran@hotmail.com>
2022-05-15 22:24:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,43 +7,37 @@ import matplotlib.pyplot as plt
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+
 def evoked(epochs):
-
-
-    evoked = epochs.average()
     
+    e1 = epochs[1].average()
+    evokeds_subset=e1
+    evokeds_subset.pick('eeg')
+    report = mne.Report(title='Evoked example')
+    report.add_evokeds(
+        evokeds=evokeds_subset,
+        titles=['evoked 1' # Manually specify titles
+                ],
+        n_time_points=5
+    )
+    report.save('report_evoked.html', overwrite=True)
     # == SAVE FILE ==
     evoked.save(os.path.join('out_dir', 'evoked-epo.fif'))
 
-    # == FIGURES ==
-    plt.figure()
-    evoked.plot()
-    plt.savefig(os.path.join('out_figs', 'evoked_plot.png'))
-
-
-    
- 
     return evoked
 
 
-
-
 def main():
-
-
     # Load inputs from config.json
     with open('config.json') as config_json:
         config = json.load(config_json)
 
     # Read the epoch file
     data_file = config.pop('fif')
-
-
-    # crop() the Raw data to save memory:
-    epochs = mne.io.read_raw_fif(data_file, verbose=False).crop(tmax=60)
-
-    evoked = evoked(epochs)
-
+    epochs = mne.read_epochs(data_file , preload=False)
+    evok = evoked(epochs)
 
 if __name__ == '__main__':
     main()
+
+
